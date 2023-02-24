ad83f729c41be5bd83e475664a474fa6b844462b
1uciuszzz <nick_zzhao@outlook.com>
2023-02-24 16:32:26 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import {
   Color,
   Rectangle,
   GeoJsonDataSource,
+  Ellipsoid,
+  Camera,
 } from "cesium";
 import { useRef } from "react";
 import { useEffect } from "react";
@@ -27,16 +29,17 @@ const App = () => {
   });
 
   useEffect(() => {
+    Camera.DEFAULT_VIEW_RECTANGLE = Rectangle.fromDegrees(90, -20, 110, 90);
     const viewer = new Viewer(cesiumContainer.current, CESIUM_CONFIG);
-    viewer.scene.globe.depthTestAgainstTerrain = true;
-    viewer.scene.screenSpaceCameraController.enableCollisionDetection = false;
     viewer.scene.globe.translucency.enabled = true;
-    viewer.scene.globe.undergroundColor = undefined;
-    viewer.scene.globe.translucency.frontFaceAlpha = 1;
-    viewer.scene.globe.translucency.backFaceAlpha = 0;
-    viewer.scene.screenSpaceCameraController.inertiaSpin = 0.1;
-    viewer.scene.screenSpaceCameraController.inertiaTranslate = 0.1;
-    viewer.scene.screenSpaceCameraController.inertiaZoom = 0.1;
+    // viewer.scene.globe.depthTestAgainstTerrain = true;
+    // viewer.scene.globe.undergroundColor = undefined;
+    // viewer.scene.globe.translucency.frontFaceAlpha = 1;
+    // viewer.scene.globe.translucency.backFaceAlpha = 0;
+    viewer.scene.screenSpaceCameraController.enableCollisionDetection = false;
+    // viewer.scene.screenSpaceCameraController.inertiaSpin = 0.1;
+    // viewer.scene.screenSpaceCameraController.inertiaTranslate = 0.1;
+    // viewer.scene.screenSpaceCameraController.inertiaZoom = 0.1;
 
     // 添加模型
     const models = [];
@@ -46,12 +49,11 @@ const App = () => {
         position: Cartesian3.fromDegrees(item.positionLon, item.positionLat),
         model: {
           uri: item.uri,
-          rotation: Math.toRadians(45),
         },
       });
       models.push(model);
     });
-    viewer.flyTo(models.at(0));
+    // viewer.flyTo(models.at(0));
 
     // 绑定鼠标移动获取坐标
     viewer.screenSpaceEventHandler.setInputAction(
@@ -66,37 +68,47 @@ const App = () => {
           const latitude = Math.toDegrees(cartographic.latitude);
           const height =
             viewer.camera.positionCartographic.height - cartographic.height;
-          setLocation({ height, latitude, longitude });
+          setLocation({ latitude, longitude, height });
         }
       }, 1000),
       ScreenSpaceEventType.MOUSE_MOVE
     );
 
-    let entities;
-    var promise = GeoJsonDataSource.load("Model/export.geojson", {
-      clampToGround: true,
-    });
-    promise.then(function (dataSource) {
-      var shenzhenhousevalue = viewer.dataSources.add(dataSource);
-      entities = dataSource.entities.values;
-      viewer.zoomTo(entities);
-      for (var i = 0; i < entities.length; i++) {
-        var entity = entities[i];
-        entity.polygon.extrudedHeight = 40;
-        entity.polygon.name = entity._name;
-        entity.polygon.material = Color.GAINSBORO;
-        entity.polygon.outline = false;
-        entity.polygon.outlineColor = Color.GAINSBORO;
-      }
-    });
+    // let entities;
+    // var promise = GeoJsonDataSource.load("Model/export.geojson", {
+    //   clampToGround: true,
+    // });
+    // promise.then(function (dataSource) {
+    //   var shenzhenhousevalue = viewer.dataSources.add(dataSource);
+    //   entities = dataSource.entities.values;
+    //   viewer.zoomTo(entities);
+    //   for (var i = 0; i < entities.length; i++) {
+    //     var entity = entities[i];
+    //     entity.polygon.extrudedHeight = 40;
+    //     entity.polygon.name = entity._name;
+    //     entity.polygon.material = Color.GAINSBORO;
+    //     entity.polygon.outline = false;
+    //     entity.polygon.outlineColor = Color.GAINSBORO;
+    //   }
+    // });
 
-    // 初始化面板
+    // 初始化面板;
     const pane = new Pane();
     pane.addInput(viewer.scene.globe.translucency, "frontFaceAlpha", {
       label: "地表透明度",
       max: 1,
       min: 0,
       step: 0.1,
+    });
+    pane.addButton({ title: "定位至中国" }).on("click", () => {
+      viewer.camera.flyTo({
+        destination: Cartesian3.fromDegrees(109.6, 32.6, 6500000.0),
+      });
+    });
+    pane.addButton({ title: "定位至深圳" }).on("click", () => {
+      viewer.camera.flyTo({
+        destination: Cartesian3.fromDegrees(114.0, 22.5, 100000.0),
+      });
     });
   }, []);
 
@@ -111,7 +123,7 @@ const App = () => {
           纬度：<span className="font-mono">{location.latitude}</span>
         </p>
         <p>
-          视角海拔：<span className="font-mono">{location.height}</span>
+          高度：<span className="font-mono">{location.height}</span>
         </p>
       </div>
     </div>
